d1a2242df790cff68ae6cac79413cb5d5b268785
indy2013 <indypenders2013@gmail.com>
2022-10-19 11:03:46 +0200

    add

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "ghcr.io/indypxl/iot-taak2/hallo:sha-26d56a9"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "iotTask2"
+  must_run = false
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
